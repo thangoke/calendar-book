d963d4fd09cac2c29bfaf831348778de278afa7f
thangok <thangnv@citics.vn>
2021-07-10 14:14:19 +0700

    [booking] define booking Entity

---
+++ src/main/java/com/thangnv/booking/controller/MeetingRoomController.java
@@ -0,0 +1,7 @@
+package com.thangnv.booking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/meeting-room")
+public class MeetingRoomController {
+}
