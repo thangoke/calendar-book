cfa4c9ef57073804f05556efe19835d7f523e0b3
thangok <thangnv@citics.vn>
2021-07-10 13:48:01 +0700

    [booking] add booking service

--- src/main/java/com/thangnv/booking/controller/BookingController.java
+++ src/main/java/com/thangnv/booking/controller/BookingController.java
@@ -1,10 +1,15 @@
 package com.thangnv.booking.controller;
 
+import com.thangnv.booking.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/booking")
 public class BookingController {
+
+    @Autowired
+    BookingService bookingService;
 
     @GetMapping("/hello")
     public String hello() {
---
+++ src/main/java/com/thangnv/booking/service/impl/BookingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.thangnv.booking.service.impl;
+
+import com.thangnv.booking.service.BookingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingServiceImpl implements BookingService {
+}
