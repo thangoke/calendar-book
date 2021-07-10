ad463048e718ba6801026c7ba25ca60e1f566ca9
thangok <thangnv@citics.vn>
2021-07-10 13:44:09 +0700

    [booking] hello world

--- src/main/java/com/thangnv/booking/controller/BookingController.java
+++ src/main/java/com/thangnv/booking/controller/BookingController.java
@@ -1,4 +1,13 @@
 package com.thangnv.booking.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
