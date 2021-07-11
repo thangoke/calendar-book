ef356e7108852b1e2c1f15cd429181076ffaa6fd
thangok <thangnv@citics.vn>
2021-07-11 20:39:20 +0700

    [booking] implement Accessory service

--- src/main/java/com/thangnv/booking/entity/Accessory.java
+++ src/main/java/com/thangnv/booking/entity/Accessory.java
@@ -1,6 +1,6 @@
 package com.thangnv.booking.entity;
 
-import org.hibernate.annotations.Type;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -8,8 +8,21 @@ import javax.persistence.*;
 @Table(name = "accessory")
 public class Accessory {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Type(type = "uuid-char")
-    @Column(name = "id", columnDefinition = "VARCHAR(255)", insertable = false, updatable = false, nullable = false)
-    private String id;
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {@org.hibernate.annotations.Parameter(name = "sequence_name", value = "accessory_seq")}
+    )
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accessory_type_id", nullable = false)
+    private AccessoryType accessoryType;
 }
