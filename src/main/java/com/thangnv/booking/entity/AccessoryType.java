0bb64de436e67cf53b475dcbf10ce5c1030b57a2
thangok <thangnv@citics.vn>
2021-07-11 20:14:46 +0700

    [booking] implement AccessoryType service

--- src/main/java/com/thangnv/booking/entity/AccessoryType.java
+++ src/main/java/com/thangnv/booking/entity/AccessoryType.java
@@ -1,15 +1,46 @@
 package com.thangnv.booking.entity;
 
-import org.hibernate.annotations.Type;
+import com.thangnv.booking.entity.audit.AuditableEntity;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "accessory_type")
-public class AccessoryType {
+public class AccessoryType extends AuditableEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Type(type = "uuid-char")
-    @Column(name = "id", columnDefinition = "VARCHAR(255)", insertable = false, updatable = false, nullable = false)
-    private String id;
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {@Parameter(name = "sequence_name", value = "accessory_type_seq")}
+    )
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
