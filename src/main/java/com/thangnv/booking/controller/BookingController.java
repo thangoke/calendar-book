c9b2706c7ebfcebffbe5e17b56a2a3b3e5977696
thangok <thangnv@citics.vn>
2021-07-12 22:51:52 +0700

    [booking] implement booking service

--- src/main/java/com/thangnv/booking/controller/BookingController.java
+++ src/main/java/com/thangnv/booking/controller/BookingController.java
@@ -1,10 +1,10 @@
 package com.thangnv.booking.controller;
 
+import com.thangnv.booking.dto.BookingRequestDTO;
+import com.thangnv.booking.dto.BookingResponseDTO;
 import com.thangnv.booking.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -16,5 +16,15 @@ public class BookingController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/book")
+    BookingResponseDTO book(@RequestBody BookingRequestDTO dto) {
+        return bookingService.book(dto);
+    }
+
+    @DeleteMapping("/cancel")
+    void cancel(@RequestParam Long id) {
+        bookingService.cancel(id);
     }
 }
